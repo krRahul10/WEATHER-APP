301533949f0ed706fd8c2033d789300ed6ff6b7f
Rahul <rahul10kr7@gmail.com>
2022-09-24 19:23:34 +0530

    static path done

--- expressWeb/src/app.js
+++ expressWeb/src/app.js
@@ -1,25 +1,33 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const app = express()
+const PORT = 5000;
 
-const PORT = 5000
+// ****PUBLIC state path*****
 
-app.get("/", (req,res) => {
-    res.send("welcome to home page")
-})
+const static_path = path.join(__dirname, "../public");
 
-app.get("/about", (req,res) => {
-    res.send("welcome to about page")
-})
+app.use(express.static(static_path));
 
-app.get("/weather", (req,res) => {
-    res.send("welcome to weather page")
-})
+// *****Routing*****
 
-app.get("*", (req,res) => {
-    res.send("404 error page ")
-})
+app.get("/", (req, res) => {
+  res.send("welcome to home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("welcome to about page");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("welcome to weather page");
+});
+
+app.get("*", (req, res) => {
+  res.send("404 error page ");
+});
 
 app.listen(PORT, () => {
-    console.log(`server started at ${PORT}`)
-})
+  console.log(`server started at ${PORT}`);
+});
