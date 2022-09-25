5a8351e957c238afd6288b8517e7d5a9a01b515d
Rahul <rahul10kr7@gmail.com>
2022-09-25 14:45:56 +0530

    frondend done

--- expressWeb/public/js/main.js
+++ expressWeb/public/js/main.js
@@ -1,6 +1,8 @@
 const submitBtn = document.getElementById('submitBtn')
-
-const getInfo = () => {
+// let api = `https://api.openweathermap.org/data/2.5/weather?q=pune&appid=a8e163bc0b34c412cb3cb3aff29558b2`;
+const getInfo = (e) => {
+    
+e.preventDefault()
 alert('hello')
 }
 
