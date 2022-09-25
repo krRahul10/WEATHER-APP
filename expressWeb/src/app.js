80e380bc3a80cc755ea6b0e3f47d6f0497b7b472
Rahul <rahul10kr7@gmail.com>
2022-09-25 12:41:24 +0530

    views done

--- expressWeb/src/app.js
+++ expressWeb/src/app.js
@@ -7,25 +7,32 @@ const PORT = 5000;
 // ****PUBLIC state path*****
 
 const static_path = path.join(__dirname, "../public");
+console.log(path.join(__dirname,"../templates"))
+const templatePath = path.join(__dirname,"../templates")
 
-app.use(express.static(static_path));
+
+// app to btatna padega ki ab hum view engine ka code use kar rhe h
+
+app.set("view engine", "hbs");
+app.set("views", templatePath)
+// app.use(express.static(static_path));
 
 // *****Routing*****
 
 app.get("/", (req, res) => {
-  res.send("welcome to home page");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.send("welcome to about page");
+  res.render("about");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("welcome to weather page");
+  res.render("weather");
 });
 
 app.get("*", (req, res) => {
-  res.send("404 error page ");
+  res.render("404error");
 });
 
 app.listen(PORT, () => {
